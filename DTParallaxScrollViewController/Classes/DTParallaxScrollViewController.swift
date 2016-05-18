671063568067da2f23b18133ce408a90159b1229
Tung Vo <DreamBroker@Tungs-MacBook-Pro.local>
2016-05-18 11:15:15 +0300

    save arezzo files

--- DTParallaxScrollViewController/Classes/DTParallaxScrollViewController.swift
+++ DTParallaxScrollViewController/Classes/DTParallaxScrollViewController.swift
@@ -6,8 +6,109 @@
 //
 //
 
-import Cocoa
+import UIKit
 
-class DTParallaxScrollViewController: UIViewController {
-
+///
+/// Class DTParallaxScrollViewController
+///
+public class DTParallaxScrollViewController: UIViewController, UIScrollViewDelegate {
+    ///
+    /// Delegate
+    ///
+    public weak var delegate: DTParallaxScrollViewDelegate? {
+        //Remove old header view before adding new one
+        set {
+            _parallaxScrollView.parallaxDelegate = newValue
+        }
+        
+        get {
+            return _parallaxScrollView.parallaxDelegate
+        }
+    }
+    
+    ///
+    /// Parallax scroll view
+    /// Note: parallaxScrollView access could be removed in the future.
+    ///
+    private var _parallaxScrollView: DTParallaxScrollView!
+    public var parallaxScrollView: DTParallaxScrollView! {
+        return _parallaxScrollView
+    }
+    
+    ///
+    /// Header height
+    /// Default value is 100.
+    ///
+    public var headerHeight: CGFloat {
+        set {
+            //Update after setting new header height
+            _parallaxScrollView.headerHeight = newValue
+        }
+        
+        get {
+            return _parallaxScrollView.headerHeight
+        }
+    }
+    
+    ///
+    /// External scroll view
+    /// scrollEnabled should not be set to true for DTParallaxScrollViewController to work properly.
+    /// The only way to set external scroll view is via initializer so it is safe here.
+    ///
+    private var _externalScrollView: UIScrollView! {
+        didSet {
+            _parallaxScrollView = DTParallaxScrollView(frame: CGRectZero, scrollView: _externalScrollView)
+        }
+    }
+    
+    public var externalScrollView: UIScrollView! {
+        return _externalScrollView
+    }
+    
+    ///
+    /// Update block based on content offset of internal scroll view.
+    /// CGFloat is vertical offet of internal scroll view.
+    /// Bool indicates if header view is visible from scroll view.
+    ///
+    public var updateBlock: ((CGFloat, Bool) -> Void)? {
+        set {
+            _parallaxScrollView.updateBlock = newValue
+        }
+        
+        get {
+            return _parallaxScrollView.updateBlock
+        }
+    }
+    
+    public init(scrollView: UIScrollView, headerHeight: CGFloat) {
+        super.init(nibName: nil, bundle: nil)
+        
+        //Do common init
+        _commonInit(scrollView)
+        
+        self.headerHeight = headerHeight
+    }
+    
+    public init(scrollView: UIScrollView) {
+        super.init(nibName: nil, bundle: nil)
+        
+        //Do common init
+        _commonInit(scrollView)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func _commonInit(scrollView: UIScrollView) {
+        _externalScrollView = scrollView
+    }
+    
+    //MARK: UIViewController
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        _parallaxScrollView.frame = self.view.bounds
+        self.view.addSubview(_parallaxScrollView)
+    }
 }
